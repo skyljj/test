b77d0ae3704035180c99d8ed0a722a5a7aad7f3d
skyljj <47238699+skyljj@users.noreply.github.com>
2026-01-22 14:28:55 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,15 @@
 find . -name "*.yaml" -exec sed -i '/^support-os:/,/^[^[:space:]]/d' {} +
+
+
+for ns in $(oc get managedclusters -o jsonpath='{.items[*].metadata.name}'); do
+  oc get secret ${ns}-admin-kubeconfig -n ${ns}
+done
+
+
+for ns in $(oc get managedclusters -o jsonpath='{.items[*].metadata.name}'); do
+  oc get secret ${ns}-admin-kubeconfig \
+    -n ${ns} \
+    -o yaml > ${ns}-admin-kubeconfig.yaml
+done
+
+oc apply -f *-admin-kubeconfig.yaml
